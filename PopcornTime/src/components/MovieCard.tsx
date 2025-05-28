e9c0c0361353f737773ae3744b99a658c921e82e
asphodel42 <asphodell42@gmail.com>
2025-05-29 00:13:22 +0300

    created prop for MovieCard

--- PopcornTime/src/components/MovieCard.tsx
+++ PopcornTime/src/components/MovieCard.tsx
@@ -1,30 +1,27 @@
 import "../index.css";
+import type { Movie } from "../../types/movie";
 
-function MovieCard() {
+function MovieCard({ posterUrl, title, description, genre, runtime }: Movie) {
   return (
     <div className="movie-cd-ct flex flex-col w-full bg-surface-a10 rounded-2xl duration-300 ease-in-out cursor-pointer hover:scale-105">
       <div className="movie-cd-img-ct flex justify-center mb-2">
         <img
-          src="https://images.fandango.com/ImageRenderer/200/0/redesign/static/img/default_poster--dark-mode.png/0/images/masterrepository/Fandango/239382/karatekidlegends-posterart.jpg"
-          alt=""
+          src={posterUrl}
+          alt={title}
           className="w-full lg:h-65 sm:h-50 h-40 object-cover rounded-t-2xl"
         />
       </div>
       <div className="px-4 pb-2 md:px-6 md:pb-4 lg:px-6 lg:pb-6">
         <h1 className="text-xl font-bold truncate text-primary-a10 mb-2">
-          Karate Kid: Legends (2025)
+          {title}
         </h1>
-        <p className="text-light-a0 mb-2 line-clamp-2">
-          Karate Kid: Legends unites the iconic martial arts masters of one of
-          the most beloved film franchises of all time to tell a completely new
-          story full of action and heart.
-        </p>
+        <p className="text-light-a0 mb-2 line-clamp-2">{description}</p>
         <div className="flex justify-between">
           <p className="text-surface-tonal-a50">
-            Genre: <br /> Action, Comedy
+            Genre: <br /> {genre}
           </p>
           <p className="text-surface-tonal-a50 ">
-            Runtime: <br /> 2h 10m
+            Runtime: <br /> {runtime}
           </p>
         </div>
       </div>
---
+++ PopcornTime/types/movie.ts
@@ -0,0 +1,8 @@
+export interface Movie {
+  id: number;
+  title: string;
+  description: string;
+  genre: string;
+  runtime: string;
+  posterUrl: string;
+}
