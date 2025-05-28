a8114d1f6caac743b0ce812d337f3ebd20c8d0ac
asphodel42 <asphodell42@gmail.com>
2025-05-28 16:29:02 +0300

    installed and inited basic dependencies

--- PopcornTime/src/App.tsx
+++ PopcornTime/src/App.tsx
@@ -1,7 +1,12 @@
+import "./App.css";
+import "./index.css";
+import { ToastContainer } from "react-toastify";
+
 function App() {
   return (
     <div>
-      <h1>Мій кінотеатр</h1>
+      <h1 className="text-5xl font-bold">PopcornTime</h1>
+      <ToastContainer />
     </div>
   );
 }
