fe8c7333f32077867a5c3843e37c4bb4fc2ab770
asphodel42 <asphodell42@gmail.com>
2025-05-29 01:08:25 +0300

    updated MovieCard and props

--- PopcornTime/src/components/MovieCard.tsx
+++ PopcornTime/src/components/MovieCard.tsx
@@ -1,27 +1,31 @@
-import "../index.css";
 import type { Movie } from "../../types/movie";
+import "../index.css";
 
-function MovieCard({ posterUrl, title, description, genre, runtime }: Movie) {
+interface Props {
+  movie: Movie;
+}
+
+function MovieCard({ movie }: Props) {
   return (
-    <div className="movie-cd-ct flex flex-col w-full bg-surface-a10 rounded-2xl duration-300 ease-in-out cursor-pointer hover:scale-105">
-      <div className="movie-cd-img-ct flex justify-center mb-2">
+    <div className="movie-cd-ct flex flex-col w-full bg-surface-a10 rounded-2xl overflow-hidden duration-300 ease-in-out cursor-pointer hover:scale-105">
+      <div className="movie-cd-img-ct w-full aspect-[2/3] overflow-hidden mb-2">
         <img
-          src={posterUrl}
-          alt={title}
-          className="w-full lg:h-65 sm:h-50 h-40 object-cover rounded-t-2xl"
+          src={movie.posterUrl}
+          alt={movie.title}
+          className="w-auto h-full rounded-t-2xl"
         />
       </div>
       <div className="px-4 pb-2 md:px-6 md:pb-4 lg:px-6 lg:pb-6">
         <h1 className="text-xl font-bold truncate text-primary-a10 mb-2">
-          {title}
+          {movie.title}
         </h1>
-        <p className="text-light-a0 mb-2 line-clamp-2">{description}</p>
-        <div className="flex justify-between">
-          <p className="text-surface-tonal-a50">
-            Genre: <br /> {genre}
+        <p className="text-light-a0 mb-2 line-clamp-2">{movie.description}</p>
+        <div className="flex justify-between text-surface-tonal-a50 text-sm">
+          <p>
+            Genre: <br /> {movie.genreNames.slice(0, 2).join(", ")}
           </p>
-          <p className="text-surface-tonal-a50 ">
-            Runtime: <br /> {runtime}
+          <p>
+            Release: <br /> {movie.releaseDate.split("-")[0]}
           </p>
         </div>
       </div>
--- PopcornTime/types/movie.ts
+++ PopcornTime/types/movie.ts
@@ -2,7 +2,8 @@ export interface Movie {
   id: number;
   title: string;
   description: string;
-  genre: string;
-  runtime: string;
+  genreNames: string[];
+  releaseDate: string;
   posterUrl: string;
+  rating: number;
 }
